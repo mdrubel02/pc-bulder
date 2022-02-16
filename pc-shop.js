0ac319014ae57efb25fa4365972aa3668a742b0c
mdrubel02 <mdrubel072125@gmail.com>
2022-02-16 10:45:19 +0600

    added

--- pc-shop.js
+++ pc-shop.js
@@ -3,4 +3,18 @@ document.getElementById('eightGB').addEventListener('click', function(){
     console.log('clicked');
     const memoryCost=document.getElementById('memory-cost');
     memoryCost.innerText=300;
+    const bestPrice=document.getElementById('best-price').innerText;
+    // console.log(bestPrice.innerText);
+    const memoryPrice=document.getElementById('memory-cost').innerText;
+    const storagePrice=document.getElementById('storage-cost').innerText;
+    const deliveryPrice=document.getElementById('delivery-cost').innerText;
+
+    // totalPrice addition 
+    const totalPrice=parseInt(bestPrice) + parseInt(memoryPrice) + parseInt(storagePrice) + parseInt(deliveryPrice)
+    console.log(totalPrice);
+
+    const grantTotal=document.getElementById('total-price').innerText=totalPrice;
+
+
+
 })
